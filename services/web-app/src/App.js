47857f2825092663f1da39dd8bcf7642e770762f
logamanyan <146450819+logamanyan@users.noreply.github.com>
2024-09-12 22:47:55 +0530

    Update App.js

--- services/web-app/src/App.js
+++ services/web-app/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Home from './components/Home';
 import SunglassesPreview from './components/SunglassesPreview';
@@ -13,36 +12,18 @@ import ClothesPreview from './components/ClothesPreview';
 function App() {
   return (
     <Router>
-      <div>
-        {/* Navigation */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/try/sunglasses">Try Sunglasses</Link>
-            </li>
-            <li>
-              <Link to="/try/cloth">Try Clothes</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* Route switching */}
-        <Switch>
-          <Route path="/try/cloth">
-            <ClothesPreview />
-          </Route>
-          <Route path="/try/sunglasses">
-            <SunglassesPreview />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+     <Switch>
+     <Route path="/try/cloth">
+       <ClothesPreview/>
+     </Route>              
+     <Route path="/try/sunglasses">
+       <SunglassesPreview/>
+     </Route>       
+     <Route path="/">
+       <Home/>
+     </Route>
+     </Switch>
+    </Router>   
   );
 }
 
